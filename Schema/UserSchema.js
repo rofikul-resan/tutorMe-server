e84c8da9abfeb7fd56f87b5d87ebf51e646fd758
rofikul-resan <resan6231@gmail.com>
2023-10-01 00:16:25 +0600

    user schema desing done

---
+++ Schema/UserSchema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  userImage: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    default: "student", // Default to 'student' if no role is specified
+  },
+  following: {
+    type: [
+      {
+        instructorId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        instructorName: String,
+      },
+    ],
+    default: [],
+  },
+  enrollCourse: {
+    type: [
+      {
+        courseId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+        courseName: String,
+        lastClassComplete: Number,
+      },
+    ],
+    default: [],
+  },
+  notification: {
+    type: [
+      {
+        notificationFor: {
+          type: String,
+          // This references the User model for instructors
+        },
+        updateMessage: String,
+        time: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    default: [], // Default to an empty array
+  },
+});
+
+module.exports = UserSchema;
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const UserSchema = require("./Schema/UserSchema");
 
 //middleware
 app.use(cors());
@@ -10,6 +13,19 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("TutorMe server is running ");
 });
+const dbUri = process.env.DB_URL;
+// const dbUri = "mongodb://localhost:27017";
+
+const userModel = mongoose.model("User", UserSchema);
+
+async function runDb() {
+  await mongoose.connect(`${dbUri}/tutor-Me`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("db connect successful");
+}
+runDb().catch((err) => console.log("error from log", err));
 
 app.listen(port, () => {
   console.log("server run in port", port);
